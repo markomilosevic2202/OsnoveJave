29b7dfb6743b45e685618d6bb69f0e2777acee6e
Marko Milosevic <marko.milosevic2202@gmail.com>
2022-05-13 21:13:41 +0200

    Upload

--- osnove_jave/src/main/java/p13_05/Git_Zadataka1Update.java
+++ osnove_jave/src/main/java/p13_05/Git_Zadataka1Update.java
@@ -1,6 +1,6 @@
 package p13_05;
 
-public class Git_Zadataka1 {
+public class Git_Zadataka1Update {
     public static void main(String[] args) {
         System.out.println("git clone");
         System.out.println("git status");
