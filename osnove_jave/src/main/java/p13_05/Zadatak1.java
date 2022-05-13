3b616936690b12e17980fc30bbca7980b7b593d5
Marko Milosevic <marko.milosevic2202@gmail.com>
2022-05-13 20:01:39 +0200

    sjdhfjsdf

--- osnove_jave/src/main/java/p13_05/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05/Zadatak1.java
@@ -2,6 +2,6 @@ package p13_05;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Helo borld");
+        System.out.println("Helo world");
     }
 }
