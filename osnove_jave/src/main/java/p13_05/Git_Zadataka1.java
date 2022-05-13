a4af5ee6f135a6df0cf27251185f2a162a3a2197
Marko Milosevic <marko.milosevic2202@gmail.com>
2022-05-13 20:33:32 +0200

    novi zadaci

---
+++ osnove_jave/src/main/java/p13_05/Git_Zadataka1.java
@@ -0,0 +1,11 @@
+package p13_05;
+
+public class Git_Zadataka1 {
+    public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add");
+        System.out.println("git commit -m");
+
+    }
+}
--- osnove_jave/src/main/java/p13_05/Greska.java
+++
@@ -1,7 +0,0 @@
-package p13_05;
-
-public class Greska {
-    public static void main(String[] args) {
-        System.out.println("Ovo je greska");
-    }
-}
