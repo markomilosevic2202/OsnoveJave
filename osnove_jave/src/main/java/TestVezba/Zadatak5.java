577f58f8f89664f3f28df66f7e48c4ef6eefb9a9
Marko Milosevic <marko.milosevic2202@gmail.com>
2022-05-18 08:54:13 +0200

    Dopuna Zadatka5 sa testa

--- osnove_jave/src/main/java/TestVezba/Zadatak5.java
+++ osnove_jave/src/main/java/TestVezba/Zadatak5.java
@@ -10,25 +10,35 @@ public class Zadatak5 {
 
 
         List<Integer> igrano = new ArrayList<>();
-        igrano.addAll(Arrays.asList(1, 2, 3));
+        igrano.addAll(Arrays.asList(1, 2, 3, 45, 31, 12, 5));
         List<Integer> izvuceno = new ArrayList<>();
-        izvuceno.addAll(Arrays.asList(3, 2, 1));
-        boolean uslvPetlje = true;
-       while (uslvPetlje ){
-            uporedjivanje(igrano.get(1), izvuceno);
+        izvuceno.addAll(Arrays.asList(3, 2, 1, 31, 45, 12, 5));
+        int uslvPetlje = 0;
+        for (int i = 0; i < igrano.size(); i++) {
+
+            if (uporedjivanje(igrano.get(i), izvuceno)) {
+                uslvPetlje++;
+            } else {
+                uslvPetlje = 0;
             }
-
         }
+        if (uslvPetlje == igrano.size()) {
+            System.out.println("Ovo jed dobitna kombinacija");
+        } else {
+            System.out.println("Kombinacija nije dobitna");
+        }
+    }
 
 
-    static boolean uporedjivanje(int igran, List<Integer> izvucen){
+    static boolean uporedjivanje(int igran, List<Integer> izvucen) {
         boolean uporedi = false;
         for (int i = 0; i < izvucen.size(); i++) {
-            if(igran == izvucen.get(i)){
+            if (igran == izvucen.get(i)) {
                 return uporedi = true;
             }
 
 
-        } return uporedi;
+        }
+        return uporedi;
     }
 }
